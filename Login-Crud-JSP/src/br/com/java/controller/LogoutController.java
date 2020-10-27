85c432e84dd1721f76c1a7e0c33379a92300e0f2
T.I <T.I@TI.rsatacadao.local>
2020-10-27 17:05:32 -0300

    criação do arquivo classe lougoutcontroller.java

---
+++ Login-Crud-JSP/src/br/com/java/controller/LogoutController.java
@@ -0,0 +1,38 @@
+package br.com.java.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Logout", urlPatterns = { "/Logout" })
+public class LogoutController extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+		
+		PrintWriter pwOut = response.getWriter();
+		
+		HttpSession session = request.getSession(false);
+		
+		session.invalidate();
+		
+		pwOut.print("Você saiu com sucesso");
+		
+		response.setContentType("text/html");
+		
+		RequestDispatcher view = request.getRequestDispatcher("/index.jsp");	
+		
+		view.include(request, response);
+		
+		pwOut.close();
+	}
+
+}
