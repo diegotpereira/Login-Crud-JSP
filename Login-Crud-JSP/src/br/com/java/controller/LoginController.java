e35907a3769f78311de4c4d835ea737864ae5482
Diego <diegoteixeirapereira@hotmail.com>
2020-10-27 15:37:58 -0300

    alteração no arquivo classe logincontroller.java adicionado metodos

--- Login-Crud-JSP/src/br/com/java/controller/LoginController.java
+++ Login-Crud-JSP/src/br/com/java/controller/LoginController.java
@@ -1,14 +1,18 @@
 package br.com.java.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.java.dao.UsuarioDAO;
+import br.com.java.model.Usuario;
 
 @WebServlet(name = "Login", urlPatterns = { "/Login" })
 public class LoginController extends HttpServlet {
@@ -36,5 +40,45 @@ public class LoginController extends HttpServlet {
 			redirectPage= ADMINPG;
 			request.setAttribute("usuarios", dao.listaUsuarios());
 		}
+		
+		else if (action.equalsIgnoreCase("listaUsuarios")) {
+			redirectPage= ADMINPG;
+			request.setAttribute("usuarios", dao.listaUsuarios());
+		}
+		
+		else if (action.equalsIgnoreCase("editar")) {
+			redirectPage= EDITPG;
+			int id = Integer.parseInt(request.getParameter("id"));
+			Usuario usuario = dao.getUsuarioByID(id);
+			request.setAttribute("usuario", usuario); 
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher(redirectPage);
+		view.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pwOut = response.getWriter();
+		String email = request.getParameter("email");
+		String senha =request.getParameter("senha");
+		
+		if (dao.validaLogin(email, senha)) {
+			Usuario usuario = dao.usuarioSession(email);
+			HttpSession session = request.getSession();
+			session.setAttribute("nome", usuario.getNome());
+			session.setAttribute("email", email);
+			RequestDispatcher view = request.getRequestDispatcher(BEMVINDOPG);	
+			view.forward(request, response);
+		}
+		
+		else {
+			pwOut.print("<p style=\"color:red\">Incorrect Username or Password!</p>");
+			RequestDispatcher view = request.getRequestDispatcher("/index.jsp");	
+			view.include(request, response);
+			
+		}
+		
+		pwOut.close();
 	}
 }
