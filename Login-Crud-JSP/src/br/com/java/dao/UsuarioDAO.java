79c8c2a98ee0d1af2325e71f9ec85c8c70955177
Diego <diegoteixeirapereira@hotmail.com>
2020-10-27 15:12:36 -0300

    criação da classe Usuario.dao

---
+++ Login-Crud-JSP/src/br/com/java/dao/UsuarioDAO.java
@@ -0,0 +1,42 @@
+package br.com.java.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.java.connection.DBUtil;
+
+
+
+public class UsuarioDAO {
+	
+	private Connection conn;
+	
+	public UsuarioDAO() {
+		
+		conn= DBUtil.getConnection();
+	}
+	
+	public boolean validaLogin(String usuario,String senha){
+		
+		boolean logado = false;
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement("select * from usuario where email=? and senha=?");
+			ps.setString(1, usuario);
+			ps.setString(2, senha);
+			
+			ResultSet rs = ps.executeQuery();
+			logado = rs.next();
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return logado;
+		
+	}
+
+}
